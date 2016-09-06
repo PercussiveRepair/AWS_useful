34c4bb4a1b44e4603256638668f16c72d988e5e8
Jay Harrison <jay.a.harrison@gmail.com>
2016-09-06 11:48:01 +0100

    Update create_iam_user.sh

--- create_iam_user.sh
+++ create_iam_user.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# Script to create IAM creds either from user input or cli option and gpg encrypt output
+# Script to create IAM creds either from user input or cli option
+
+set -e
 
 function usage()
 {
@@ -10,8 +12,8 @@ function usage()
     msg '    options:'
     msg '      -h this help'
     msg '      -u list of space separated users to create creds for in the form firstname.lastname'
-    msg '      -p AWS credentials profile name to use (AdministratorAccess or ReadOnlyAccess)'
-    msg '      -l level of access to grant'
+    msg '      -p list of space separated AWS credentials profile names to use'
+    msg '      -l level of access to grant (AdministratorAccess or ReadOnlyAccess)'
     msg '      -g GPG key to encrypt for'
 }
 
@@ -20,7 +22,7 @@ while getopts hu:p:l: opt; do
     case ${opt} in
         h) usage; exit 0;;
         u) USERS=${OPTARG};;
-        p) PROFILE=${OPTARG};;
+        p) PROFILES=${OPTARG};;
         l) LEVEL=${OPTARG};;
         g) GPG=${OPTARG};;
     esac
@@ -31,16 +33,16 @@ done
 shift $((OPTIND-1))
 
 # if no cli options then ask for the
-if [ $OPTIND -eq 1 ]; then
-  PROFILES=`cat ~/.aws/credentials | grep "\[" | tr '\n' ' '`
+if [[ $OPTIND -eq 1 ]]; then
+  PROFILE_LIST=`cat ~/.aws/credentials | grep "\[" | tr '\n' ' '`
   GPG_KEYS=`gpg --list-keys | grep uid | cut -d"<" -f 2 | cut -d">" -f 1`
 
   echo "Usernames, space separated"
   read USERS
 
-  echo "AWS profile name from:"
-  echo $PROFILES
-  read PROFILE
+  echo "AWS account profiles, space separated"
+  echo $PROFILE_LIST
+  read PROFILES
 
   echo "Access level - 1) AdministratorAccess or 2) ReadOnlyAccess"
   read LEVEL
@@ -48,33 +50,37 @@ if [ $OPTIND -eq 1 ]; then
   echo "GPG encrypt? y/N"
   read GPGYN
 
-  if [ $GPGYN = "y" ]; then
+  if [[ $GPGYN = "y" ]]; then
     echo "Which user?"
     echo $GPG_KEYS
     read GPG
   fi
 fi
 
-if [ $LEVEL == '1' ]; then
+if [[ $LEVEL == '1' ]]; then
   LEVEL='AdministratorAccess'
-elif [ $LEVEL == '2' ]; then
+elif [[ $LEVEL == '2' ]]; then
   LEVEL='ReadOnlyAccess'
 fi
 
 for i in $USERS; do
-  echo "Username: $i" | tee -a $i.creds
-  CREATE=`aws --profile $PROFILE iam create-user --user-name $i`
-  POLICY=`aws --profile $PROFILE iam attach-user-policy --policy-arn arn:aws:iam::aws:policy/$LEVEL --user-name $i`
-  KEYS=`aws --profile $PROFILE iam create-access-key --user-name $i`
-  PWD=`openssl rand -base64 12`
-  PASS=`aws --profile $PROFILE iam create-login-profile --user-name $i --no-password-reset-required --password $PWD`
-  ID=`echo $KEYS | cut -d "\"" -f 22`
-  KEY=`echo $KEYS | cut -d "\"" -f 18`
-  echo "Password: $PWD" | tee -a $i.creds
-  echo "AWS_ID: $ID" | tee -a $i.creds
-  echo "AWS_SECRET: $KEY" | tee -a $i.creds
-  echo "URL: https://$PROFILE.signin.aws.amazon.com/console" | tee -a $i.creds
-  gpg -e --trust-model always -r $GPG -o $i.$PROFILE.creds.gpg $i.creds
-  rm $i.creds
-  echo "GPG encrypted to $i.$PROFILE.creds.gpg"
+  for p in $PROFILES; do
+    echo "Username: $i" | tee -a $i.$p.creds
+    CREATE=`aws --profile $p iam create-user --user-name $i`
+    POLICY=`aws --profile $p iam attach-user-policy --policy-arn arn:aws:iam::aws:policy/$LEVEL --user-name $i`
+    KEYS=`aws --profile $p iam create-access-key --user-name $i`
+    PWD=`openssl rand -base64 12`
+    PASS=`aws --profile $p iam create-login-profile --user-name $i --no-password-reset-required --password $PWD`
+    ID=`echo $KEYS | cut -d "\"" -f 22`
+    KEY=`echo $KEYS | cut -d "\"" -f 18`
+    echo "Password: $PWD" | tee -a $i.creds
+    echo "AWS_ID: $ID" | tee -a $i.creds
+    echo "AWS_SECRET: $KEY" | tee -a $i.creds
+    echo "URL: https://$p.signin.aws.amazon.com/console" | tee -a $i.$p.creds
+    if [[ $GPGYN = "y" ]]; then
+      gpg -e --trust-model always -r $GPG -o $i.$p.creds.gpg $i.creds
+      rm $i.$p.creds
+      echo "GPG encrypted to $i.$p.creds.gpg"
+    fi
   done
+done
