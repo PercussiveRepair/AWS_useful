c8f4e12f2bd01e8c2b8a8915020949753df4e84d
Jay Harrison <jay.a.harrison@gmail.com>
2016-10-31 11:23:55 +0000

    Update instance_events_to_slack.py

--- instance_events_to_slack.py
+++ instance_events_to_slack.py
@@ -17,15 +17,15 @@ try:
   for line in creds:
     if line.startswith('['):
       profile=line[1:-2]
-      print profile
+      # print profile
       for region in regions:
         boto3.setup_default_session(profile_name=profile, region_name=region)
         client = boto3.client('ec2')
         for status in client.describe_instance_status(Filters=filters)['InstanceStatuses']:
           instance = client.describe_instances(InstanceIds=[status['InstanceId']])
-          instancename = 'Undefined'
-          instancerole = 'Undefined'
-          instanceproduct = 'Undefined'
+          instancename = ''
+          instancerole = ''
+          instanceproduct = ''
           if 'Tags' in instance['Reservations'][0]['Instances'][0]:
             for tag in instance['Reservations'][0]['Instances'][0]['Tags']:
               if tag['Key'] == 'Name':
@@ -34,18 +34,25 @@ try:
                 instancerole = tag['Value']
               elif tag['Key'] == 'product':
                 instanceproduct = tag['Value']
-          instanceinfo = { 'Account': profile, 'InstanceId': status['InstanceId'], 'Action': status['Events'][0]['Code'], 'Status': status['Events'][0]['Description'], 'Name': instancename, 'Role': instancerole, 'Product': instanceproduct, 'Region/AZ': status['AvailabilityZone'], 'DueDate': status['Events'][0]['NotBefore']}
+          date = str(status['Events'][0]['NotBefore'])[:-15]
+          description = str(status['Events'][0]['Description']).replace('The instance is running on ', '')
+          action = str(status['Events'][0]['Code']).replace('system-', '').replace('instance-', '')
+  
+          instanceinfo = { 'Account': profile, 'InstanceId': status['InstanceId'], 'Action': action, 'Status': description, 'Name': instancename, 'Region/AZ': status['AvailabilityZone'], 'DueDate': date}
+  
           instanceevents.append(instanceinfo)
+
 except Exception,e: 
   print str(e)
   pass
-  
-# assemble slack message
-table = tabulate(instanceevents, headers='keys') 
-message = '```' + table + '```'
 
-sc = SlackClient(token)
-print sc.api_call(
-        "chat.postMessage", channel="<slack channel>", text=message,
-        username='AWS Instance Events', icon_emoji=':bomb:'
-)
+# assemble slack message  
+table = tabulate(instanceevents, headers='keys') 
+message = '``` ' + table + ' ```'
+
+if instanceevents:
+  sc = SlackClient(token)
+  print sc.api_call(
+          "chat.postMessage", channel="<slack channel>", text=message,
+          username='AWS Instance Events', icon_emoji=':bomb:'
+  )
